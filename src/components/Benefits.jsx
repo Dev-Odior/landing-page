38b3ef4cb13ce257455292ed3a9d656279038496
Dev-Odior <godspoweraino@gmail.com>
2024-06-27 14:03:32 +0100

    hopeful

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,6 +4,7 @@ import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
+import card from "../assets/benefits/cardOne.svg";
 
 const Benefits = () => {
   return (
@@ -18,11 +19,9 @@ const Benefits = () => {
           {benefits.map((item) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={
-                {
-                  backgroundImage: `url(${item.backgroundUrl})`,
-                }
-              }
+              style={{
+                backgroundImage: `url(${card})`,
+              }}
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
